3d5e80339f2dee8f0fb2a47fbcd971c61a908a49
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-01 10:34:43 +0800

    1368B: Complete Cpp AC

--- Cpp/1368b.cpp
+++ Cpp/1368b.cpp
@@ -14,28 +14,21 @@ double better_floor(double x)
     return (y - x < EPS ? y : floor(x));
 }
 
-int calc_m1(long long unsigned k, int a)
+int calc_m(long long unsigned k, int a)
 {
-    double a1, a2, a3, a4, a5;
+    long long unsigned l = 1;
+    int m = 10;
 
-    a1 = log(a);
-    a2 = log(a + 1);
-    a3 = 10 * a2;
-    a4 = log(k);
-    a5 = (a3 - a4) / (a2 - a1);
+    for (int i = 0; i < m; ++i)
+    {
+        l *= a;
+    }
+    while (l < k && m-- >= 0)
+    {
+        l = l * (a + 1) / a;
+    }
 
-    return int(better_floor(a5));
-}
-
-int calc_m2(long long unsigned k, int a)
-{
-    double a1, a2, a3;
-
-    a1 = pow(a + 1, 10) / k;
-    a2 = (a + 1) * 1.0 / a;
-    a3 = log(a1) / log(a2);
-
-    return int(better_floor(a3));
+    return m;
 }
 
 int main()
@@ -45,17 +38,16 @@ int main()
 
     cin >> k;
 
-    a = int(pow(k, 0.1));
-    // m = calc_m1(k, a);
-    m = calc_m2(k, a);
+    a = int(better_floor(pow(k, 0.1)));
+    m = calc_m(k, a);
 
-    for (int i = 0; i < m; ++i)
-    {
-        cout << string(a, base[i]);
-    }
-    for (int i = m; i < 10; ++i)
+    for (int i = 0; i < 10 - m; ++i)
     {
         cout << string(a + 1, base[i]);
+    }
+    for (int i = 10 - m; i < 10; ++i)
+    {
+        cout << string(a, base[i]);
     }
 
     return 0;
