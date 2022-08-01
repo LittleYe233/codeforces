a2124259225bb7d07c6d17a9257fb396076351b7
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-02 00:38:28 +0800

    1399D: Complete Cpp AC

--- Cpp/1399d.cpp
+++ Cpp/1399d.cpp
@@ -1,42 +1,76 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 #include <vector>
 
 using namespace std;
+
+#ifdef DEBUG
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << '[';
+    if (!v.empty())
+    {
+        for (auto it = v.begin(); it != v.end() - 1; ++it)
+        {
+            os << *it << ", ";
+        }
+        cout << v.back();
+    }
+    os << ']';
+
+    return os;
+}
+#endif
 
 char change_bit(char x)
 {
     return x == '1' ? '0' : '1';
 }
 
+int change_bit(int x)
+{
+    return x ? 0 : 1;
+}
+
+int get_bit(char x)
+{
+    return x - '0';
+}
+
 void solve_and_print(string s)
 {
-    int n = s.length();
-    vector<int> ans(n, 0);
-    vector<char> ss;
+    int n = s.length(), total = 0;
+    vector<int> ans(n, 0), stack[2];
 
     for (int i = 0; i < n; ++i)
     {
-        bool found = false;
+#ifdef DEBUG
+        cout << "i        = " << i << endl;
+#endif
         char c = change_bit(s[i]);
-        for (int j = 0; j < ss.size(); ++j)
+        int x = get_bit(c), y = get_bit(s[i]);
+        if (stack[x].empty())
         {
-            if (ss[j] == c)
-            {
-                found = true;
-                ss[j] = s[i];
-                ans[i] = j + 1; // answer should start with 1
-                break;
-            }
+            ++total;
+            ans[i] = total;
+            stack[y].push_back(total - 1);
         }
-        if (!found)
+        else
         {
-            ss.push_back(s[i]);
-            ans[i] = ss.size();
+            int j = stack[x].back();
+            stack[x].pop_back();
+            stack[y].push_back(j);
+            ans[i] = j + 1;
         }
+#ifdef DEBUG
+        cout << "stack[0] = " << stack[0] << endl;
+        cout << "stack[1] = " << stack[1] << endl;
+        cout << "ans      = " << ans << endl;
+#endif
     }
 
-    cout << ss.size() << endl;
+    cout << total << endl;
     for (int i = 0; i < n; ++i)
     {
         cout << ans[i] << ' ';
@@ -46,6 +80,9 @@ void solve_and_print(string s)
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
     int t, n;
     string s;
 
